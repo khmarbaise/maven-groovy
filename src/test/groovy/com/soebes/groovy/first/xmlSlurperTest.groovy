fb2ec78c9ccfdc912498f896ab08f261930ee34c
Karl Heinz Marbaise <info@soebes.de>
2012-03-11 15:03:04 +0100

    - Unit Test in Groovy.

--- src/test/groovy/com/soebes/groovy/first/xmlSlurperTest.groovy
+++ src/test/groovy/com/soebes/groovy/first/xmlSlurperTest.groovy
@@ -1,16 +1,28 @@
 package com.soebes.groovy.first
+
+import groovy.util.GroovyTestCase
 import groovy.util.XmlSlurper
 
-def pom = new XmlSlurper().parse(new File('pom.xml'))
+class xmlSlurperTest extends GroovyTestCase {
 
-def params = [ key1:'value1', key2:'value2', key3:'value3 ']
+    void testFirst() {
+	
+	def pom = new XmlSlurper().parse(new File('pom.xml'))
+		
+        //def params = [ key1:'value1', key2:'value2', key3:'value3 ']
+        //
+        //println params.collect { k, v -> "$k=$v" }.join('&')
+        //
+        //pom.project.each { child -> println child }
+        //
+        //for (module in pom.project.modules) {
+        //    println "${module.@name}"
+        //}
+        //
+        //println "Hello here i am."
+		
+	println pom.size()
+	println pom.@project.size()
+    }
 
-println params.collect { k, v -> "$k=$v" }.join('&')
-
-pom.project.each { child -> println child }
-
-for (module in pom.project.modules) {
-    println "${module.@name}"
 }
-
-println "Hello here i am."
