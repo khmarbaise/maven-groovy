dcb67d8eec0d4d3d5540055da53a974dea952c4e
Karl Heinz Marbaise <info@soebes.de>
2013-10-06 19:00:08 +0200

    - TestNG unit test and Groovy Unit Test.

---
+++ src/test/java/com/soebes/groovy/first/AnOtherTest.java
@@ -0,0 +1,14 @@
+package com.soebes.groovy.first;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.testng.annotations.Test;
+
+public class AnOtherTest {
+
+  @Test
+  public void firstTest() {
+    assertThat(true).isTrue();
+  }
+
+}
